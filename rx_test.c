749541b65847e4fb5175f5091125468b7e31aa10
AndresFernandez <73385513+Fernandezaginer@users.noreply.github.com>
2024-03-15 16:38:25 +0100

    Add test rx c

---
+++ rx_test.c
@@ -0,0 +1,39 @@
+
+
+#include <stdio.h>
+#include <wiringPi.h>
+
+#define DEVICE_UART "/dev/ttyS0"
+#define SPEED_UART 9600
+
+
+int main(void){
+
+  if (wiringPiSetup() == -1){
+	printf("Error wiringPiSetup() \n");
+	return -1;
+  }
+
+  int fd;
+  if (fd = serialOpen(DEVICE_UART, SPEED_UART)) < 0){
+	printf("serialOpen(DEVICE_UART, \n");
+	return -1;
+  }
+
+  int i = 0;
+  
+  while(1){
+
+	  if (serialDataAvail(fd)){
+		if (i == 0){
+		  printf("\n RX: ");
+		}
+		printf("%d", serialGetchar(fd));
+		i++;
+		if (i > 90){
+		   i = 0;	
+		}
+	  }	    
+  }
+  return 0;
+}
