30a9444900d8c1bc6946da15687bfc22fc0c99ce
AndresFernandez <73385513+Fernandezaginer@users.noreply.github.com>
2024-03-28 13:13:07 +0100

    Auto set serial

--- nerfnet/net/primary_radio_interface.h
+++ nerfnet/net/primary_radio_interface.h
@@ -1,12 +1,12 @@
 /*
  * Copyright 2020 Andrew Rossignol andrew.rossignol@gmail.com
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,42 +18,48 @@
 #define NERFNET_NET_PRIMARY_RADIO_INTERFACE_H_
 
 #include <optional>
-
 #include "nerfnet/net/radio_interface.h"
 
-namespace nerfnet {
+#define SPEED_UART 115200
 
-// The primary mode radio interface.
-class PrimaryRadioInterface : public RadioInterface {
- public:
-  // Setup the primary radio link.
-  PrimaryRadioInterface(uint16_t ce_pin, int tunnel_fd,
-                        uint32_t primary_addr, uint32_t secondary_addr,
-                        uint8_t channel, uint64_t poll_interval_us);
+#define DEFAULT_UART "/dev/serial0"
+#define RPI5_UART "/dev/ttyAMA0"
+#define RPI5_HOST "raspberrypi5"
 
-  // Runs the interface.
-  void Run();
+namespace nerfnet
+{
 
- private:
-  // The interval between poll operations to the secondary radio.
-  const uint64_t poll_interval_us_;
+  // The primary mode radio interface.
+  class PrimaryRadioInterface : public RadioInterface
+  {
+  public:
+    // Setup the primary radio link.
+    PrimaryRadioInterface(uint16_t ce_pin, int tunnel_fd,
+                          uint32_t primary_addr, uint32_t secondary_addr,
+                          uint8_t channel, uint64_t poll_interval_us);
 
-  // Logic for poll backoff when the secondary radio is not responding.
-  int poll_fail_count_;
-  uint64_t current_poll_interval_us_;
-  bool connection_reset_required_;
+    // Runs the interface.
+    void Run();
 
-  // Requests that a new connection be opened.
-  bool ConnectionReset();
+  private:
+    // The interval between poll operations to the secondary radio.
+    const uint64_t poll_interval_us_;
 
-  // Sends and receives messages to exchange network packets.
-  bool PerformTunnelTransfer();
+    // Logic for poll backoff when the secondary radio is not responding.
+    int poll_fail_count_;
+    uint64_t current_poll_interval_us_;
+    bool connection_reset_required_;
 
-  // Updates the backoff configuration in the light of a failure.
-  void HandleTransactionFailure();
+    // Requests that a new connection be opened.
+    bool ConnectionReset();
 
-};
+    // Sends and receives messages to exchange network packets.
+    bool PerformTunnelTransfer();
 
-}  // namespace nerfnet
+    // Updates the backoff configuration in the light of a failure.
+    void HandleTransactionFailure();
+  };
 
-#endif  // NERFNET_NET_PRIMARY_RADIO_INTERFACE_H_
+} // namespace nerfnet
+
+#endif // NERFNET_NET_PRIMARY_RADIO_INTERFACE_H_
