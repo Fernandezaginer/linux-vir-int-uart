e4b0b2e4e47b1fbf4f9981eb48405b9b61a5900d
AndresFernandez <73385513+Fernandezaginer@users.noreply.github.com>
2024-03-29 11:24:33 +0100

    Test

--- nerfnet/net/nerfnet_main.cc
+++ nerfnet/net/nerfnet_main.cc
@@ -18,6 +18,65 @@
 constexpr char kDescription[] = "Mesh networking for NRF24L01 radios.";
 constexpr char kVersion[] = "0.0.1";
 
+// Sets flags for a given interface. Quits and logs the error on failure.
+void SetInterfaceFlags(const std::string_view &device_name, int flags)
+{
+  int fd = socket(AF_INET, SOCK_DGRAM, 0);
+  CHECK(fd >= 0, "Failed to open socket: %s (%d)", strerror(errno), errno);
+
+  struct ifreq ifr = {};
+  ifr.ifr_flags = flags;
+  strncpy(ifr.ifr_name, std::string(device_name).c_str(), IFNAMSIZ);
+  int status = ioctl(fd, SIOCSIFFLAGS, &ifr);
+  CHECK(status >= 0, "Failed to set tunnel interface: %s (%d)",
+        strerror(errno), errno);
+  close(fd);
+}
+
+void SetIPAddress(const std::string_view &device_name,
+                  const std::string_view &ip, const std::string &ip_mask)
+{
+  int fd = socket(AF_INET, SOCK_DGRAM, 0);
+  CHECK(fd >= 0, "Failed to open socket: %s (%d)", strerror(errno), errno);
+
+  struct ifreq ifr = {};
+  strncpy(ifr.ifr_name, std::string(device_name).c_str(), IFNAMSIZ);
+
+  ifr.ifr_addr.sa_family = AF_INET;
+  CHECK(inet_pton(AF_INET, std::string(ip).c_str(),
+                  &reinterpret_cast<struct sockaddr_in *>(&ifr.ifr_addr)->sin_addr) == 1,
+        "Failed to assign IP address: %s (%d)", strerror(errno), errno);
+  int status = ioctl(fd, SIOCSIFADDR, &ifr);
+  CHECK(status >= 0, "Failed to set tunnel interface ip: %s (%d)",
+        strerror(errno), errno);
+
+  ifr.ifr_netmask.sa_family = AF_INET;
+  CHECK(inet_pton(AF_INET, std::string(ip_mask).c_str(),
+                  &reinterpret_cast<struct sockaddr_in *>(&ifr.ifr_netmask)->sin_addr) == 1,
+        "Failed to assign IP mask: %s (%d)", strerror(errno), errno);
+  status = ioctl(fd, SIOCSIFNETMASK, &ifr);
+  CHECK(status >= 0, "Failed to set tunnel interface mask: %s (%d)",
+        strerror(errno), errno);
+  close(fd);
+}
+
+// Opens the tunnel interface to listen on. Always returns a valid file
+// descriptor or quits and logs the error.
+int OpenTunnel(const std::string_view &device_name)
+{
+  int fd = open("/dev/net/tun", O_RDWR);
+  CHECK(fd >= 0, "Failed to open tunnel file: %s (%d)", strerror(errno), errno);
+
+  struct ifreq ifr = {};
+  ifr.ifr_flags = IFF_TUN | IFF_NO_PI;
+  strncpy(ifr.ifr_name, std::string(device_name).c_str(), IFNAMSIZ);
+
+  int status = ioctl(fd, TUNSETIFF, &ifr);
+  CHECK(status >= 0, "Failed to set tunnel interface: %s (%d)",
+        strerror(errno), errno);
+  return fd;
+}
+
 int main(int argc, char **argv)
 {
   TCLAP::CmdLine cmd(kDescription, ' ', kVersion);
@@ -45,6 +104,13 @@ int main(int argc, char **argv)
   // Setup the network.
   nerfnet::NetworkManager network_manager;
   network_manager.RegisterTransport<nerfnet::RadioTransport>(std::move(link));
+
+  int tunnel_fd = OpenTunnel("nerf0");
+  LOGI("tunnel '%s' opened", "nerf0");
+  SetInterfaceFlags("nerf0", IFF_UP);
+  LOGI("tunnel '%s' up", "nerf0");
+  SetIPAddress("nerf0", tunnel_ip, "255.255.255.0");
+  LOGI("tunnel '%s' configured with '%s' mask '%s'", "nerf0", "192.168.10.1", "255.255.255.0");
 
   // TODO(aarossig): Start the network manager and block until quit.
   while (1)
