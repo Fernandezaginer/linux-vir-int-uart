77b3a7daa161799e7f8943f0ef1847f011ae58d9
AndresFernandez <73385513+Fernandezaginer@users.noreply.github.com>
2024-04-16 18:25:04 +0200

    Test delay yield CPU

--- nerfnet/util/time.cc
+++ nerfnet/util/time.cc
@@ -1,12 +1,12 @@
 /*
  * Copyright 2020 Andrew Rossignol andrew.rossignol@gmail.com
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,30 +19,37 @@
 #include <chrono>
 #include <unistd.h>
 
-namespace nerfnet {
+namespace nerfnet
+{
 
-void SleepUs(uint64_t delay) {
-  usleep(delay);
-}
+  void SleepUs(uint64_t delay)
+  {
+    std::this_thread::sleep_for(std::chrono::microseconds(delay));
+  }
 
-uint64_t TimeNowUs() {
-  return std::chrono::duration_cast<std::chrono::microseconds>(
-            std::chrono::steady_clock::now().time_since_epoch()).count();
-}
+  uint64_t TimeNowUs()
+  {
+    return std::chrono::duration_cast<std::chrono::microseconds>(
+               std::chrono::steady_clock::now().time_since_epoch())
+        .count();
+  }
 
-uint64_t RealClock::TimeNowUs() const {
-  return ::nerfnet::TimeNowUs();
-}
+  uint64_t RealClock::TimeNowUs() const
+  {
+    return ::nerfnet::TimeNowUs();
+  }
 
-MockClock::MockClock()
-    : time_us_(0) {}
+  MockClock::MockClock()
+      : time_us_(0) {}
 
-uint64_t MockClock::TimeNowUs() const {
-  return time_us_;
-}
+  uint64_t MockClock::TimeNowUs() const
+  {
+    return time_us_;
+  }
 
-void MockClock::SetTimeUs(uint64_t time_us) {
-  time_us_ = time_us;
-}
+  void MockClock::SetTimeUs(uint64_t time_us)
+  {
+    time_us_ = time_us;
+  }
 
-}  // namespace nerfnet
+} // namespace nerfnet
