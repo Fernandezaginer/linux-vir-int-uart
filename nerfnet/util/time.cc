bb7ab0d46457604fdc7b05f1a5b23e470ca4cf4e
AndresFernandez <73385513+Fernandezaginer@users.noreply.github.com>
2024-04-16 18:29:13 +0200

    Update

--- nerfnet/util/time.cc
+++ nerfnet/util/time.cc
@@ -1,12 +1,12 @@
 /*
  * Copyright 2020 Andrew Rossignol andrew.rossignol@gmail.com
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,15 +19,19 @@
 #include <chrono>
 #include <unistd.h>
 
-namespace nerfnet {
+namespace nerfnet
+{
 
-void SleepUs(uint64_t delay) {
-  usleep(delay);
-}
+  void SleepUs(uint64_t delay)
+  {
+    std::this_thread::sleep_for(std::chrono::microseconds(delay));
+  }
 
-uint64_t TimeNowUs() {
-  return std::chrono::duration_cast<std::chrono::microseconds>(
-            std::chrono::steady_clock::now().time_since_epoch()).count();
-}
+  uint64_t TimeNowUs()
+  {
+    return std::chrono::duration_cast<std::chrono::microseconds>(
+               std::chrono::steady_clock::now().time_since_epoch())
+        .count();
+  }
 
-}  // namespace nerfnet
+} // namespace nerfnet
