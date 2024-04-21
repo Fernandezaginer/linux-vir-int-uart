7219dd82443a705e7f2e24a57f91949b9e8b4409
AndresFernandez <73385513+Fernandezaginer@users.noreply.github.com>
2024-04-21 10:29:27 +0200

    Test

--- nerfnet/net/radio_interface.cc
+++ nerfnet/net/radio_interface.cc
@@ -71,7 +71,7 @@ namespace nerfnet
 
     // serialPrintf(fd, request.data());
 
-    serialPuts(fd, static_cast<const char *>(request.data()));
+    serialPuts(fd, reinterpret_cast<const char *>(request.data()));
     // return RequestResult::Success;
 
     // serialPuts(fd, static_cast<const char *>(request));
